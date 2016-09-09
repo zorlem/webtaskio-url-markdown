ccaf28ba48d35848c57236c2cf96a6d1d9fcc080
Hristo Erinin <hristo.erinin@gmail.com>
2016-09-09 10:24:20 -0600

    Stop requesting results in JSON as the API is buggy
    * There is a bug with the service - when JSON output is requested the content is not fully converted to markdown. Switch the API to plain markdown.
    * escape the URL string

--- webtasksio-snippets/url-to-markdown.js
+++ webtasksio-snippets/url-to-markdown.js
@@ -1,14 +1,15 @@
 var request = require('request');
+var qs = require('querystring');
 
 module.exports = function (ctx, cb) {
 
     var targeturl = ctx.data.url;
 
-    var markdownurl = 'http://fuckyeahmarkdown.com/go/?output=json&u='
-    var url = markdownurl + targeturl;
+    var markdownurl = 'http://fuckyeahmarkdown.com/go/?output=markdown&u='
+    var url = markdownurl + qs.escape(targeturl);
     request.get({url: url}, function(error, response, body) {
         if(!error && response.statusCode == 200) {
-            cb(null, JSON.parse(body));
+            cb(null, body);
         } else {
             console.log('Url: ' + url + 'Error: ' + error + ' Response code: ' + response.statusCode);
             return cb(error);
